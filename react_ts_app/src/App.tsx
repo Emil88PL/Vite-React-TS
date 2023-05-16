822b31b035cf99c703048eb61f52266a9a94b5d3
Emil <emil88pl@gmail.com>
2023-05-17 00:58:09 +0100

    finding a good teacher is difficult...

--- react_ts_app/src/App.tsx
+++ react_ts_app/src/App.tsx
@@ -6,11 +6,19 @@ import ListGroup from './Components/ListGroup';
 
 
 function App() {
+  const listgroup = [
+    'Wash the dishes',
+    'Walk the dog',
+    'Clean the widnow',
+    'Put shirt on',
+    'That fella do not know how to explain... ',
+     "code is changing and he dint't say why lol"
+];
 
   return (
     <>
       <Message />
-      <ListGroup />
+      <ListGroup items={listgroup} headings="Cities"/>
     </>
   )
 }
--- react_ts_app/src/Components/ListGroup.tsx
+++ react_ts_app/src/Components/ListGroup.tsx
@@ -1,26 +1,43 @@
+//import { MouseEvent } from "react";
+
+import { useState } from "react";
 
 
-function ListGroup() {
 
 
-    const listgroup = [
-        'Wash the dishes',
-        'Walk the dog',
-        'Clean the widnow',
-        'Put shirt on'
-    ];
+    // Interface
+    // {items: [], headings: string}
+    interface Props {
+        items: string[], 
+        headings: string
+    }
 
-    const listItems = listgroup.map(  // use unique ID generator - (like in NPM packages) UUID and Shortid 
-        (item, index) => <li key={index} className="list-group-item" onClick={(e) => console.log("CLICKED!!!", {index},{item}, {e})}>{item}</li>
-    );
+    function ListGroup({items, headings}: Props) {
+
+   // using props we can pass data to our components
+
+ 
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+    // data that can change over time
+    
+
+    //const [name, setName] = useState(" ");
+
+    // const handleClick = (e: MouseEvent, item: string, index: number) => {
+    //     console.log("CLICKED!!!", item, index, e);
+    // };
+
+    const listItems = items.map((item, index) => (
+        <li key={index} className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} onClick={() => {setSelectedIndex(index)}}>{item}</li>
+    ));
+
     // SyntheticEvent (e) - Event (clientX, clientY - place where click happened), type "click", target name etc...
 
+    
     return (
         <>
-            <h2>Group List </h2>
-            <ul className="list-group">
-                {listItems}
-            </ul>
+            <h2>{headings}</h2>
+            <ul className="list-group">{listItems}</ul>
         </>
     );
 
